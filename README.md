1f3c6738e85d756988e28e9a062dfe1c8af391f6
matheecs <matheecs@qq.com>
2024-02-14 15:46:37 +0800

    add `README.md` and `rename_tools.py`

---
+++ README.md
@@ -0,0 +1,8 @@
+# Albums
+
+## convert `heic` to `jpg`
+
+```shell
+magick mogrify -monitor -format jpg *.heic
+magick mogrify -monitor -format jpg *.HEIC
+```
---
+++ rename_tools.py
@@ -0,0 +1,42 @@
+import os
+from datetime import datetime
+
+topics = ['chongqing', 'foods', 'h1', 'hangzhou_life',
+          'marriage', 'phd', 'travel', 'xiangyang']
+
+
+for topic in topics:
+    # Directory containing the images
+    directory = "/Users/zhangjixiang/Code/AJ_Vol9/images/" + topic
+
+    # Check if the directory exists
+    if not os.path.isdir(directory):
+        print(f"Directory {directory} not found.")
+        exit(1)
+
+    # Change directory to the target directory
+    os.chdir(directory)
+
+    # Get a list of all image files
+    image_files = [file for file in os.listdir() if file.lower().endswith(
+        ('.jpg', '.JPG', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp'))]
+
+    # Loop through all image files
+    for file in image_files:
+        # Get the latest modified time of the file
+        modified_time = os.path.getmtime(file)
+
+        # Format the modified time as YYYYMMDDHHMMSS
+        formatted_time = datetime.utcfromtimestamp(
+            modified_time).strftime('%Y%m%d%H%M%S')
+
+        # Extract the file extension
+        _, extension = os.path.splitext(file)
+
+        # Construct the new filename with modified time and extension
+        new_filename = f"{formatted_time}{extension}"
+
+        # Rename the file
+        os.rename(file, new_filename)
+
+        print(f"Renamed '{file}' to '{new_filename}'")
